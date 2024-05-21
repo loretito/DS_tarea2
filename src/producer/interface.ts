2b7ea916a07fbaa68b87161b8dfaead41e5e7e27
Loreto <loreto.nancucheo@gmail.com>
2024-05-21 00:03:18 -0400

    chore: :fire: Remove unnecessary files and code

--- src/producer/interface.ts
+++
@@ -1,6 +0,0 @@
-export interface ProductData {
-  id: number;
-  name: string;
-  price: number;
-  email: string;
-}
--- src/producer/kafka.ts
+++
@@ -1,6 +0,0 @@
-import { Kafka } from 'kafkajs';
-
-export const kafka = new Kafka({
-  clientId: 'kafkaProducer',
-  brokers: ['localhost:9092', 'localhost:9092'],
-});
