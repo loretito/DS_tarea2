1120b091909c4806ccce118b93ea2e3f2dde2bdf
Loreto <loreto.nancucheo@gmail.com>
2024-05-25 14:01:41 -0400

    feat: :white_check_mark: Update providers

--- src/producer/app.module.ts
+++ src/producer/app.module.ts
@@ -4,11 +4,23 @@ import { SendTopicService } from './sendTopic.service';
 import { CreateTopicService } from './createTopic.service';
 import { KafkaModule } from 'src/kafka/kafka.module';
 import { DeliveryRequestConsumer } from '../consumer/deliveryRequest.consumer';
+import { DeliveredTopicService } from './deliveredTopic.service';
+import { RequestStatusService } from './requestStatus.service';
+import { CompleteTopicService } from './completeTopic.service';
+import { PreparedTopicService } from './preparedTopic.service';
 
 @Module({
   imports: [KafkaModule],
   controllers: [AppController],
-  providers: [CreateTopicService, SendTopicService, DeliveryRequestConsumer],
+  providers: [
+    CreateTopicService,
+    SendTopicService,
+    DeliveryRequestConsumer,
+    DeliveredTopicService,
+    RequestStatusService,
+    CompleteTopicService,
+    PreparedTopicService,
+  ],
 })
 export class AppModule {
   constructor(private readonly creaTopicService: CreateTopicService) {
