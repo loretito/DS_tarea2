708f85ea94a70b3d6988b3ef1ec127c54fc3bf27
Loreto <loreto.nancucheo@gmail.com>
2024-05-25 14:00:44 -0400

    feat: :white_check_mark: Store request in db

--- interface.ts
+++ interface.ts
@@ -1,7 +1,9 @@
+export type Status = 'RECEIVED' | 'PREPARED' | 'DELIVERED' | 'COMPLETE';
+
 export interface ProductData {
   id?: number;
   name: string;
   price: number;
   email: string;
-  status?: string;
+  status?: Status;
 }
--- src/consumer/deliveryRequest.consumer.ts
+++ src/consumer/deliveryRequest.consumer.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConsumerService } from '../kafka/consumer.service';
 import { ProductData } from 'interface';
-//import sql from './db-connection';
+import sql from './db-connection';
 
 @Injectable()
 export class DeliveryRequestConsumer implements OnModuleInit {
@@ -14,8 +14,10 @@ export class DeliveryRequestConsumer implements OnModuleInit {
         eachMessage: async ({ topic, partition, message }) => {
           const productData: ProductData = JSON.parse(message.value.toString());
           productData.status = 'RECEIVED';
-          // await this.saveProduct(productData);
+          const order = await this.saveProduct(productData);
+
           console.log({
+            order_id: order,
             value: message.value.toString(),
             topic: topic.toString(),
             partition: partition.toString(),
@@ -24,16 +26,21 @@ export class DeliveryRequestConsumer implements OnModuleInit {
       },
     );
   }
-  // private async saveProduct(product: ProductData) {
-  //   const { name, price, email, status } = product;
-  //   try {
-  //     await sql`
-  //     INSERT INTO "order" (product_name, price, email, status)
-  //     VALUES (${name}, ${price}, ${email}, ${status});
-  //   `;
-  //     console.log(`Recibido producto: ${name}`);
-  //   } catch (error) {
-  //     console.error(`Error saving product: ${error}`);
-  //   }
-  // }
+  private async saveProduct(product: ProductData) {
+    const { name, price, email, status } = product;
+    console.log('holaa');
+    try {
+      console.log(`Momento guardar: ${name}`);
+      const id = await sql`
+      INSERT INTO "order" (product_name, price, email, "status")
+      VALUES (${name}, ${price}, ${email}, ${status})
+      returning bd_id;
+    `;
+      console.log(`Recibido producto: ${name}`);
+
+      return id;
+    } catch (error) {
+      console.error(`Error saving product: ${error}`);
+    }
+  }
 }
